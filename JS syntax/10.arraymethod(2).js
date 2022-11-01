88ae6aa601b6400f912a357aef09b2f1bdff224b
AUDWO <kmjstj3@naver.com>
2022-11-01 19:26:35 +0900

    Add JS syntax 2022-11-01

--- JS syntax/10.arraymethod(2).js
+++ JS syntax/10.arraymethod(2).js
@@ -71,7 +71,7 @@ console.log(members.size); // 0
 //최초에 추가된 순서를 유지하면서,
 //나중에 중복된 값을 추가하려고 하면 그 값은 무시하는 특징이 있다.
 
-const numbers = [1, 3, 4, 3, 3, 3, 2, 1, 1, 1, 5, 5, 3, 2, 1, 4];
+const numbers = [1, 3, 4, 3, 3, 3, 2, 1, 1, 1, 5,  3, 2, 1, 4];
 const uniqNumbers = new Set(numbers);
 
 console.log(uniqNumbers); // Set(5) {1, 3, 4, 2, 5}
---
+++ JS syntax/modern property (4).js
@@ -0,0 +1,61 @@
+    const title = 'Codeit';
+
+const user1 = {
+	title,
+};
+
+console.log(user); //>>> {title:'Codeit'}
+
+//------------------------------------------
+
+function get () {
+
+	return `${this.firstName}`
+}
+
+const user2 = {
+	firstName: 'Tess',
+	get,
+};
+
+console.log(user) // 'Tess'
+
+//---------------------------
+//property name과 value로 사용할 변수나 함수의 이름이 같다면 하만 작
+function geetfull() {
+	return `${this.firstName} ${this.lastName}`;
+};
+
+const user3 = {
+	firstName: 'Tess',
+	lastName: 'Jang',
+	getFullName,
+};
+
+//--------------------------
+
+
+const user4 = {
+	firstName: 'Tess',
+	lastName: 'Jang',
+	getFullName() {
+		return `${this.firstName} ${this.lastName}`;
+	},
+};
+
+console.log(user.getFullName());
+
+//계산된 속성명 (computed property name)
+const propertyName = 'birth';
+const getJob = () => 'job';
+
+const codeit = {
+	['topic' + 'Name']: 'Modern JavaScript',
+	[propertyName]:2017,
+	[getJob()]:'프로그래밍 강사'
+	//[getJob()]는 함수의 return값을 나타낸
+};
+//codeit[propertyName]=codeit.birth
+//codeit[getJob()]=codeit.job
+console.log(codeit); //>>>{ topicName:"Moder JavaScript", ~~~}
+
