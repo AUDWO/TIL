1ae86434badd98f6b4a8f4272be57e0ef6d8b05c
AUDWO <kmjstj3@naver.com>
2022-11-08 21:25:28 +0900

    Add JS syntax 2022-11-08

---
+++ JS syntax/17.REST API (incomplete).js
@@ -0,0 +1,12 @@
+//REST API:오늘날 많은 웹 개발자들이 WEB API 설계를 할 때, 준수하기 위해 노력하는 일종의 가이드 라인
+
+//REST architecture
+
+//1.client-Server:구조를 통해 양측의 관심사를 분리
+
+//2.Stateless: Client가 보낸 각 리퀘스트에 관해서 Server는 그 어떤 맥락(context)도 
+//저장하지 않기때문에 리퀘스트에는 항상 필요한 모든 정보가 담겨야 한다.
+
+//3.Cache: Client가 Server와 통신하는 
+
+//resource:웹상에 존재하는 데이터를 나타내는 표
